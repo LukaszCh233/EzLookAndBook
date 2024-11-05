beceea9b591b18bd9eeb8d562a99cee5c4661bff
LukaszCh233 <103947294+LukaszCh233@users.noreply.github.com>
2024-11-05 14:43:51 +0100

    Initial commit

---
+++ src/main/java/EzLookAndBook/EzLookAndBookApplication.java
@@ -0,0 +1,13 @@
+package EzLookAndBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzLookAndBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzLookAndBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/EzLookAndBook/EzLookAndBookApplicationTests.java
@@ -0,0 +1,13 @@
+package EzLookAndBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EzLookAndBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
